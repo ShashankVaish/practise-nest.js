9fa13514654633edb39802d042bbb04ed88a7412
Your Name <you@example.com>
2025-08-03 23:11:57 +0530

    feat: add AuthDto for user authentication and implement signup logic with password hashing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
@@ -8,8 +9,9 @@ export class AuthController {
         return this.authService.signin();
     }
     @Post('signup')
-    signup() {
-        return this.authService.signup();
+    signup(@Body() dto:AuthDto) {
+        console.log(dto)
+        return this.authService.signup(dto);
     }
 
     
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
 @Injectable()
 export class AuthService {
-    constructor(prisma : PrismaService) {}
+    constructor( private readonly prisma : PrismaService) {}
     signin(){
         return "signin";
 
     }
-    signup(){
-        return "signup";
-    }
+    async signup(dto:AuthDto){
+        //password is convert into hash 
+        const hash = await argon2.hash(dto.password);
+        // save user to db
+        const user = await this.prisma.user.create({
+            data:{
+                email: dto.email,
+                hash,
+            }
+        })
+        // return user
+        // delete user.hash; // remove password hash from the response
+        return user;
+    }   
+        
 }
