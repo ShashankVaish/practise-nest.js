05045e6ae5b0e4f38d132e5798a19296f4750076
Your Name <you@example.com>
2025-08-08 19:40:12 +0530

    feat: refactor signup method to handle errors and improve password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 @Injectable()
 export class AuthService {
     constructor( private readonly prisma : PrismaService) {}
@@ -11,7 +12,8 @@ export class AuthService {
     }
     async signup(dto:AuthDto){
         //password is convert into hash 
-        const hash = await argon2.hash(dto.password);
+        try {
+            const hash = await argon2.hash(dto.password);
         // save user to db
         const user = await this.prisma.user.create({
             data:{
@@ -22,6 +24,17 @@ export class AuthService {
         // return user
         // delete user.hash; // remove password hash from the response
         return user;
+            
+        }catch (error) {
+            if( error instanceof PrismaClientKnownRequestError ) {
+                if( error.code === 'P2002' ) {
+                    // unique constraint failed
+                    throw new Error('User credentials already taken');
+                }
+                
+            }
+            throw new Error('Something went wrong');
+        }
     }   
         
 }
